7168fb8d0592444301b29ebb018614c15d03aaeb
HBUTHUANGPX <2082672018@qq.com>
2023-12-01 01:53:57 +0800

    1130 double serial

---
+++ build/serial_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hpx/serial_ros/src/serial_test
+# Build directory: /home/hpx/serial_ros/build/serial_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
