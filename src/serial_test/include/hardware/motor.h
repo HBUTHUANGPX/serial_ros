f04dfe4768d74e109a7397dbce754dd85f2f4286
HBUTHUANGPX <2082672018@qq.com>
2023-12-02 16:21:39 +0800

    修改了motor.h增加了无参数send方法

--- src/serial_test/include/hardware/motor.h
+++ src/serial_test/include/hardware/motor.h
@@ -10,12 +10,12 @@ private:
     int type, id, CANport_num, CANboard_num;
     ros::NodeHandle n;
     lively_serial *ser;
+    cdc_acm_rx_message_t cmd;
 
 public:
     motor(int _motor_num, int _CANport_num, int _CANboard_num, lively_serial *_ser) : ser(_ser), CANport_num(_CANport_num), CANboard_num(_CANboard_num)
     {
-        if (n.getParam("robot/CANboard/No_" + std::to_string(_CANboard_num) + "_CANboard/CANport/CANport_" + std::to_string(_CANport_num) + "/motor/motor" + std::to_string(_motor_num) + "/id",
-                       id))
+        if (n.getParam("robot/CANboard/No_" + std::to_string(_CANboard_num) + "_CANboard/CANport/CANport_" + std::to_string(_CANport_num) + "/motor/motor" + std::to_string(_motor_num) + "/id", id))
         {
             // ROS_INFO("Got params id: %d",id);
         }
@@ -23,10 +23,7 @@ public:
         {
             ROS_ERROR("Faile to get params id");
         }
-        if (n.getParam("robot/CANboard/No_" + std::to_string(_CANboard_num) +
-                           "_CANboard/CANport/CANport_" + std::to_string(_CANport_num) +
-                           "/motor/motor" + std::to_string(_motor_num) + "/type",
-                       type))
+        if (n.getParam("robot/CANboard/No_" + std::to_string(_CANboard_num) + "_CANboard/CANport/CANport_" + std::to_string(_CANport_num) + "/motor/motor" + std::to_string(_motor_num) + "/type", type))
         {
             // ROS_INFO("Got params type: %d",type);
         }
@@ -34,6 +31,9 @@ public:
         {
             ROS_ERROR("Faile to get params type");
         }
+        cmd.motor_cmd.ID = id;
+        cmd.head[0] = 0xFE;
+        cmd.head[1] = 0xFD;
     }
     ~motor() {}
     template <typename T>
@@ -55,7 +55,25 @@ public:
     }
     void send(float position, float velocity, float torque, float Kp, float Kd)
     {
-        ser->send(id, float2int<int32_t>(position,1), float2int<int32_t>(velocity,1), float2int<int32_t>(torque,2), float2int<int16_t>(Kp,3), float2int<int16_t>(Kp,3));
+        ser->send(id, float2int<int32_t>(position, 1),
+                  float2int<int32_t>(velocity, 1),
+                  float2int<int32_t>(torque, 2),
+                  float2int<int16_t>(Kp, 3),
+                  float2int<int16_t>(Kp, 3));
+    }
+    /*无参数send 需要先调用fresh_cmd,然后调用此函数发送*/
+    void send()
+    {
+        ser->send(&cmd);
+    }
+    void fresh_cmd(float position, float velocity, float torque, float Kp, float Kd)
+    {
+        cmd.motor_cmd.position = float2int<int32_t>(position, 1);
+        cmd.motor_cmd.velocity = float2int<int32_t>(velocity, 1);
+        cmd.motor_cmd.torque = float2int<int32_t>(torque, 2);
+        cmd.motor_cmd.Kp = float2int<int16_t>(Kp, 3);
+        cmd.motor_cmd.Kd = float2int<int16_t>(Kp, 3);
+        cmd.crc16 = crc_ccitt(0x0000, (const uint8_t *)&cmd, sizeof(cdc_acm_rx_message_t) - 2);
     }
     int get_motor_id() { return id; }
     int get_motor_type() { return type; }
--- src/serial_test/src/serial_t.cpp
+++ src/serial_test/src/serial_t.cpp
@@ -3,19 +3,6 @@
 #include "../include/hardware/robot.h"
 #include <iostream>
 #include <thread>
-
-void read_params()
-{
-    ros::NodeHandle n;
-    std::string robot_name;
-    // if (n.getParam("robot/robot_name",robot_name)){ROS_INFO("Got params: %s",robot_name.c_str());}else{ROS_ERROR("Faile to get params");}
-    // int CANboard_num;
-    // if (n.getParam("robot/CANboard",CANboard_num)){ROS_INFO("Got params: %d",CANboard_num);}else{ROS_ERROR("Faile to get params");}
-    // for (size_t i = 0; i < CANboard_num; i++)
-    // {
-    //     /* code */
-    // }
-}
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "my_serial_port");
@@ -35,29 +22,21 @@ int main(int argc, char **argv)
     }
     ros::Rate r(100);
     robot rb(&ser);
-
     ROS_INFO("\033[1;32mSTART\033[0m");
-
     while (ros::ok())
     {
         ROS_INFO_STREAM("START");
         for (motor m : rb.Motors)
         {
-            m.send(0, 0, 0, 30000, 0);
+            m.send(0.0, 0.0, 0.0, 0.0, 0.0);
             r.sleep();
         }
         ROS_INFO_STREAM("END"); // STEP2 -> END 1.7ms  START -> END 1.71
     }
-    //////
     for (auto &thread : threads)
     {
         thread.join();
     }
-
     ros::spin();
-    // a.send(0x04,0,0,0,30000,0);
-    // a.send(0x03,0,0,0,30000,0);
-    // a.send(0x02,0,0,0,30000,0);
-    // a.send(0x01,0,0,0,30000,0);
     return 0;
 }
